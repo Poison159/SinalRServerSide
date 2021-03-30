f4f55cfbcd4ff2531f502287deb13a2e6c60e7d5
Poison159 <Siyabongahdb@gmail.com>
2021-03-30 20:58:32 +0200

    first Commit

---
+++ SignalRWeb/WorkerServices/MessageBrokerPubSubWorker.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Hosting;
+using SignalRWeb.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace SignalRWeb.WorkerServices
+{
+    [Route("api/messagebroker")]
+    public sealed class MessageBrokerPubSubWorker : BackgroundService
+    {
+        private IHubContext<MessageBrokerHub> _messageBrokerHubContext;
+
+        public MessageBrokerPubSubWorker(IHubContext<MessageBrokerHub> messageBrokerHubContext) {
+            _messageBrokerHubContext = messageBrokerHubContext;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            
+            while (!stoppingToken.IsCancellationRequested) {
+                await Task.Delay(1000);
+                var proc = Process.GetCurrentProcess();
+                var mem = proc.WorkingSet64;
+                var cpu = proc.TotalProcessorTime;
+                var eventMessage = new EventMessage("memory => " + mem + " | Procesor => "+ cpu.TotalSeconds.ToString(), $"{Guid.NewGuid():N}", DateTime.Now);
+                await _messageBrokerHubContext.Clients.All.SendAsync("onMessageReceived",eventMessage,stoppingToken);
+            }
+        }
+
+    }
+}
---
+++ SignalRWeb/wwwroot/js/messagebroker.js
@@ -0,0 +1,36 @@
+﻿"use strict"
+
+const signalRConnection = new signalR.HubConnectionBuilder()
+    .withUrl("/messagebroker")
+    .build();
+
+signalRConnection.start().then(function () {
+    console.log("SignalR Connected");
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+
+signalRConnection.on("onMessageReceived", function (eventMessage) {
+    console.log(eventMessage);
+});
+
+let messageCount = 0;
+
+signalRConnection.on("onMessageReceived", function (eventMessage) {
+    messageCount++;
+    const msgCountH4 = document.getElementById("messageCount");
+    msgCountH4.innerText = "Messages: " + messageCount.toString();
+    const ul = document.getElementById("messages");
+    const li = document.createElement("li");
+    li.innerText = messageCount.toString();
+
+    for (const prop in eventMessage) {
+        const newDiv = document.createElement("div");
+        const newContent = document.createTextNode(`${prop}: ${eventMessage[prop]}`);
+        newDiv.appendChild(newContent);
+        li.append(newDiv);
+    }
+    ul.appendChild(li);
+    window.scrollTo(0, document.body.scrollHeight);
+});
